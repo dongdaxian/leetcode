9a575adf8f121dfe2f4c4362274c6c2e80ed23aa
ddx <dongdaxianlaile@qq.com>
2022-02-10 22:51:45 +0800

    fix

---
+++ src/main/java/below850/problem832/Test.java
@@ -0,0 +1,28 @@
+package below850.problem832;
+
+import java.util.Objects;
+
+public class Test {
+    public int[][] flipAndInvertImage(int[][] A) {
+        if (Objects.isNull(A)) {
+            return null;
+        }
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                if (A[i][left] == A[i][right]) {
+                    A[i][left] ^= 1;
+                    A[i][right] ^= 1;
+                }
+                left++;
+                right--;
+            }
+            if (left == right) {
+                A[i][left] ^= 1;
+            }
+        }
+        return A;
+    }
+
+}
