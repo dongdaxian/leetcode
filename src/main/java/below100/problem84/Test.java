c4ae80acf143c5c296a330cf2a17e33d5d49c5c3
didi <>
2022-02-24 22:06:01 +0800

    fix

--- src/main/java/below100/problem84/Test.java
+++ src/main/java/below100/problem84/Test.java
@@ -4,73 +4,73 @@ import java.util.Stack;
 
 public class Test {
 
-	public static void main(String[] args) {
-		System.out.println(new Test().largestRectangleArea2(new int[]{1,5,1}));
+    public static void main(String[] args) {
+        System.out.println(new Test().largestRectangleArea2(new int[]{1, 5, 1}));
 
-	}
-	
-	public int largestRectangleArea1(int[] heights) {	//∂‘”¶42Ã‚µ⁄∂˛÷÷Ω‚∑®
-		if (heights == null || heights.length == 0) return 0;
-		int n = heights.length;
-		int[] fromLeft = new int[n];
-		int[] fromRight = new int[n];
-		fromLeft[0] = -1;
-		fromRight[n - 1] = n;
-		for(int i = 1, j = 0; i < n; i++) {
-			j = i - 1;
-			while(j > -1 && heights[i] <= heights[j])	//»‘ «¡Ω≤„—≠ª∑£¨¥À¥¶”√for(;;j--)∏¥‘”∂»≤ª±‰
-				j = fromLeft[j];
-			fromLeft[i] = j;
-		}
-		for(int i = n - 2, j = n - 1; i > -1; i--) {
-			j = i + 1;
-			while(j < n && heights[i] <= heights[j])
-				j = fromRight[j];
-			fromRight[i] = j;
-		}
-		
-		int maxArea = 0;
-		for(int i = 0; i < n; i++) 
-			maxArea = Math.max(maxArea, heights[i] * (fromRight[i] - fromLeft[i] - 1));
-		return maxArea;
     }
 
-	public int largestRectangleArea2(int[] heights) {	// π”√’ª
-		if (heights == null || heights.length == 0) return 0;
-		Stack<Integer> st = new Stack<>();
-	    int maxArea = 0;
-	    for(int i = 0; i <= heights.length; i++) {
-	    	
-	    	if(st.isEmpty() || (i != heights.length && heights[i] >= heights[st.peek()]))
-	    		st.push(i);
-	    	else {
-	    		int temp = heights[st.pop()];
-	    		maxArea = Math.max(maxArea, (st.isEmpty() ? i : (i - st.peek() - 1)) * temp);
-	    		i--;
-	    	}
-	    }
-	    
-		return maxArea;
+    public int largestRectangleArea1(int[] heights) {    //ÂØπÂ∫î42È¢òÁ¨¨‰∫åÁßçËß£Ê≥ï
+        if (heights == null || heights.length == 0) return 0;
+        int n = heights.length;
+        int[] fromLeft = new int[n];
+        int[] fromRight = new int[n];
+        fromLeft[0] = -1;
+        fromRight[n - 1] = n;
+        for (int i = 1, j = 0; i < n; i++) {
+            j = i - 1;
+            while (j > -1 && heights[i] <= heights[j])    //‰ªçÊòØ‰∏§Â±ÇÂæ™ÁéØÔºåÊ≠§Â§ÑÁî®for(;;j--)Â§çÊùÇÂ∫¶‰∏çÂèò
+                j = fromLeft[j];
+            fromLeft[i] = j;
+        }
+        for (int i = n - 2, j = n - 1; i > -1; i--) {
+            j = i + 1;
+            while (j < n && heights[i] <= heights[j])
+                j = fromRight[j];
+            fromRight[i] = j;
+        }
+
+        int maxArea = 0;
+        for (int i = 0; i < n; i++)
+            maxArea = Math.max(maxArea, heights[i] * (fromRight[i] - fromLeft[i] - 1));
+        return maxArea;
     }
-	
-	public int largestRectangleArea3(int[] heights) {	//∑÷÷Œ∑®
-		return getMaxArea(heights, 0, heights.length - 1);
-	}
 
-	public int getMaxArea(int[] heights, int i, int j) {
-		if(i > j) return 0;
-		if(i == j) return heights[i];
-		int min = Integer.MAX_VALUE;
-		int ptr = 0;
-		
-		for(int k = i; k <= j; k++) {
-			if(min > heights[k]) {
-				min = heights[k];
-				ptr = k;
-			}
+    public int largestRectangleArea2(int[] heights) {    //‰ΩøÁî®Ê†à
+        if (heights == null || heights.length == 0) return 0;
+        Stack<Integer> st = new Stack<>();
+        int maxArea = 0;
+        for (int i = 0; i <= heights.length; i++) {
 
-		}
-		
-		return Math.max(min * (j - i + 1), Math.max(getMaxArea(heights, i, ptr - 1), getMaxArea(heights, ptr + 1, j)));
-	}	
+            if (st.isEmpty() || (i != heights.length && heights[i] >= heights[st.peek()]))
+                st.push(i);
+            else {
+                int temp = heights[st.pop()];
+                maxArea = Math.max(maxArea, (st.isEmpty() ? i : (i - st.peek() - 1)) * temp);
+                i--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public int largestRectangleArea3(int[] heights) {    //ÂàÜÊ≤ªÊ≥ï
+        return getMaxArea(heights, 0, heights.length - 1);
+    }
+
+    public int getMaxArea(int[] heights, int i, int j) {
+        if (i > j) return 0;
+        if (i == j) return heights[i];
+        int min = Integer.MAX_VALUE;
+        int ptr = 0;
+
+        for (int k = i; k <= j; k++) {
+            if (min > heights[k]) {
+                min = heights[k];
+                ptr = k;
+            }
+
+        }
+
+        return Math.max(min * (j - i + 1), Math.max(getMaxArea(heights, i, ptr - 1), getMaxArea(heights, ptr + 1, j)));
+    }
 }
