4bee609d06d2b4939da756b1096120da220cf402
didi <>
2022-02-25 11:50:25 +0800

    fix

--- src/main/java/below100/problem84/Test.java
+++ src/main/java/below100/problem84/Test.java
@@ -1,21 +1,25 @@
 package below100.problem84;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(new Test().largestRectangleArea2(new int[]{1, 5, 1}));
+        System.out.println(new Test().largestRectangleArea1_2(new int[]{2,1,5,6,2,3}));
 
     }
-
+    //暴力解法是O(n^2)，但从直觉来说结果复杂度应该小于排序，会有更好时间复杂度的解法
+    //类似42题第二种解法，找出每一根柱子对应的左右边界
     public int largestRectangleArea1(int[] heights) {
         if (heights == null || heights.length == 0) return 0;
         int n = heights.length;
         int[] fromLeft = new int[n];
         int[] fromRight = new int[n];
         fromLeft[0] = -1;
-        fromRight[n - 1] = n;                               //有点像42题第二种解法，但实质不同
+        fromRight[n - 1] = n;
         for (int i = 1, j = 0; i < n; i++) {                //类似于28题kmp中计算模式串的算法，时间复杂度为O(n)
             j = i - 1;
             while (j > -1 && heights[i] <= heights[j])
@@ -35,15 +39,44 @@ public class Test {
         return maxArea;
     }
 
-    public int largestRectangleArea2(int[] heights) {    //使用栈
+    //使用单调栈计算fromLeft、fromRight
+    public int largestRectangleArea1_2(int[] heights) {
+        if (heights == null || heights.length == 0) return 0;
+        int n = heights.length;
+        int[] fromLeft = new int[n];
+        int[] fromRight = new int[n];
+        Arrays.fill(fromLeft, -1);
+        Arrays.fill(fromRight, n);
+        //使用单调栈计算fromLeft、fromRight
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = n - 1; i > -1; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] > heights[i]) {
+                fromLeft[stack.pop()] = i;
+            }
+            stack.push(i);
+        }
+        stack.clear();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] > heights[i]) {
+                fromRight[stack.pop()] = i;
+            }
+            stack.push(i);
+        }
+        int maxArea = 0;
+        for (int i = 0; i < n; i++)
+            maxArea = Math.max(maxArea, heights[i] * (fromRight[i] - fromLeft[i] - 1));
+        return maxArea;
+    }
+
+    //另一种单调栈
+    public int largestRectangleArea2(int[] heights) {
         if (heights == null || heights.length == 0) return 0;
         Stack<Integer> st = new Stack<>();
         int maxArea = 0;
         for (int i = 0; i <= heights.length; i++) {
-
-            if (st.isEmpty() || (i != heights.length && heights[i] >= heights[st.peek()]))
+            if (st.isEmpty() || (i != heights.length && heights[i] >= heights[st.peek()])) {
                 st.push(i);
-            else {
+            } else {
                 int temp = heights[st.pop()];
                 maxArea = Math.max(maxArea, (st.isEmpty() ? i : (i - st.peek() - 1)) * temp);
                 i--;
