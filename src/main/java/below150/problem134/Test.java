77e3b9e6e961f158a1428bdbf81fa2a1239873fb
didi <>
2022-03-18 20:19:23 +0800

    fix

--- src/main/java/below150/problem134/Test.java
+++ src/main/java/below150/problem134/Test.java
@@ -1,31 +1,31 @@
 package below150.problem134;
 
 public class Test {
-	public int canCompleteCircuit(int[] gas, int[] cost) {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
         int[] left = new int[gas.length];
         int sum = 0;
         int ptr = -1;
-        for(int i = 0; i < gas.length; i++) {
+        for (int i = 0; i < gas.length; i++) {
             left[i] = gas[i] - cost[i];
-            if(sum == 0){
-                if(left[i] >= 0){
+            if (sum == 0) {
+                if (left[i] >= 0) {
                     sum += left[i];
                     ptr = i;
                 }
             } else {
                 sum += left[i];
-                if(sum < 0){
+                if (sum < 0) {
                     ptr = -1;
                     sum = 0;
                 }
             }
         }
-        if(ptr == -1) return -1;
+        if (ptr == -1) return -1;
         sum = 0;
-        for(int i = 0; i < gas.length; i++){
+        for (int i = 0; i < gas.length; i++) {
             int j = (i + ptr) % gas.length;
             sum += left[j];
-            if(sum < 0)
+            if (sum < 0)
                 return -1;
         }
         return ptr;
--- src/main/java/below150/problem135/Test.java
+++ src/main/java/below150/problem135/Test.java
@@ -7,48 +7,49 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Test {
-	public static void main(String[] args) {
-		System.out.println(new Test().candy2(new int[]{1, 2, 2}));
-	}
-	public int candy(int[] ratings) {
-		int bptr = 0, eptr = 0;
-		int sum = 1;
-		while(eptr < ratings.length - 1) {
-			while(eptr < ratings.length - 1 && ratings[eptr + 1] > ratings[eptr])
-				eptr++;
-			int left = eptr - bptr;
-			bptr = eptr;
-			while(eptr < ratings.length - 1 && ratings[eptr + 1] < ratings[eptr])
-				eptr++;
-			int right = eptr - bptr;
-			bptr = eptr;
-			sum += (1 + left) * left / 2 + (1 + right) * right / 2 + Integer.max(left, right);
-			while(eptr < ratings.length - 1 && ratings[eptr + 1] == ratings[eptr]) {
-				eptr++;
-				sum++;
-			}
-			bptr = eptr;
-		}
-		return sum;
+    public static void main(String[] args) {
+        System.out.println(new Test().candy2(new int[]{1, 2, 2}));
     }
-	
-	public int candy2(int[] ratings) {
-		int[] candy = new int[ratings.length];
-		Arrays.fill(candy, 1);
-		for(int i = 0; i < ratings.length - 1; i++) {
-			if(ratings[i + 1] > ratings[i]) {
-				candy[i + 1] = candy[i] + 1;
-			}
-		}
-		for(int i = ratings.length - 1; i > 0; i--) {
-			if(ratings[i - 1] > ratings[i] && candy[i - 1] <= candy[i]) {
-				candy[i - 1] = candy[i] + 1;
-			}
-		}
-		int candies = 0;
-		for(int k: candy)
-			candies += k;
-		return candies;
-	}
-	
+
+    public int candy(int[] ratings) {
+        int bptr = 0, eptr = 0;
+        int sum = 1;
+        while (eptr < ratings.length - 1) {
+            while (eptr < ratings.length - 1 && ratings[eptr + 1] > ratings[eptr])
+                eptr++;
+            int left = eptr - bptr;
+            bptr = eptr;
+            while (eptr < ratings.length - 1 && ratings[eptr + 1] < ratings[eptr])
+                eptr++;
+            int right = eptr - bptr;
+            bptr = eptr;
+            sum += (1 + left) * left / 2 + (1 + right) * right / 2 + Integer.max(left, right);
+            while (eptr < ratings.length - 1 && ratings[eptr + 1] == ratings[eptr]) {
+                eptr++;
+                sum++;
+            }
+            bptr = eptr;
+        }
+        return sum;
+    }
+
+    public int candy2(int[] ratings) {
+        int[] candy = new int[ratings.length];
+        Arrays.fill(candy, 1);
+        for (int i = 0; i < ratings.length - 1; i++) {
+            if (ratings[i + 1] > ratings[i]) {
+                candy[i + 1] = candy[i] + 1;
+            }
+        }
+        for (int i = ratings.length - 1; i > 0; i--) {
+            if (ratings[i - 1] > ratings[i] && candy[i - 1] <= candy[i]) {
+                candy[i - 1] = candy[i] + 1;
+            }
+        }
+        int candies = 0;
+        for (int k : candy)
+            candies += k;
+        return candies;
+    }
+
 }
