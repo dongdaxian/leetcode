c02b4cd62172d9df7554c32f85d483c4bbdcab71
ddx <dongdaxianlaile@qq.com>
2022-07-13 00:39:32 +0800

    fix

--- src/main/java/below50/problem18/Test.java
+++ src/main/java/below50/problem18/Test.java
@@ -5,32 +5,32 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Test {
-	public static void main(String[] args) {
-		
-	}
-	
-	public List<List<Integer>> fourSum(int[] nums, int target) {
-		Arrays.sort(nums);
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        for(int i = 0; i < nums.length - 3; i++){
-            if(i > 0 && nums[i] == nums[i - 1])
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-            for(int j = i + 1; j < nums.length - 2; j++){
-                if(j > i + 1 && nums[j] == nums[j - 1])
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
                     continue;
                 int left = j + 1, right = nums.length - 1;
                 int tempTarget = target - nums[i] - nums[j];
-                while(left < right){
-                    if(left > j + 1 && nums[left] == nums[left - 1]){
+                while (left < right) {
+                    if (left > j + 1 && nums[left] == nums[left - 1]) {
                         ++left;
                         continue;
-                    }   
-                    if(right < nums.length - 1 && nums[right] == nums[right + 1]){
+                    }
+                    if (right < nums.length - 1 && nums[right] == nums[right + 1]) {
                         --right;
                         continue;
                     }
 
-                    if(nums[left] + nums[right] == tempTarget){
+                    if (nums[left] + nums[right] == tempTarget) {
                         List<Integer> temp = new ArrayList<>();
                         temp.add(nums[i]);
                         temp.add(nums[j]);
@@ -39,9 +39,9 @@ public class Test {
                         res.add(temp);
                         ++left;
                         --right;
-                    }else if(nums[left] + nums[right] < tempTarget){
+                    } else if (nums[left] + nums[right] < tempTarget) {
                         ++left;
-                    }else{
+                    } else {
                         --right;
                     }
                 }
--- src/main/java/below500/problem454/Test.java
+++ src/main/java/below500/problem454/Test.java
@@ -5,78 +5,85 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Test {
-	
-	
-	public static void main(String[] args) {
-		System.out.println(new Test().fourSumCount3(new int[] {-1, 0, 1}, new int[] {-1, 0, 1}, new int[] {0, 0, 1}, new int[] {-1, 1, 1}));
-		
-		
-	}
-	
-	//�ݹ��֦
-	public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+
+
+    public static void main(String[] args) {
+        System.out.println(new Test().fourSumCount3(new int[]{-1, 0, 1}, new int[]{-1, 0, 1}, new int[]{0, 0, 1}, new int[]{-1, 1, 1}));
+
+
+    }
+
+    //递归剪枝
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
         Arrays.sort(A);
         Arrays.sort(B);
         Arrays.sort(C);
         Arrays.sort(D);
-        return fourSumCount(A, B, C, D, new int[] {0, 0, 0, 0}, 0);
+        return fourSumCount(A, B, C, D, new int[]{0, 0, 0, 0}, 0);
     }
-    public int fourSumCount(int[] A, int[] B, int[] C, int[] D, int[] index, int order){
+
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D, int[] index, int order) {
         int len = A.length;
-        if(index[0] >= len || index[1] >= len || index[2] >= len || index[3] >= len)
+        if (index[0] >= len || index[1] >= len || index[2] >= len || index[3] >= len)
             return 0;
         int sum = A[index[0]] + B[index[1]] + C[index[2]] + D[index[3]];
         int res = 0;
-        if(sum > 0)
+        if (sum > 0)
             res = 0;
-        else{
-        	if(sum == 0)
-        		res++;
-        	while(order < 4) {
-        		index[order]++;
-        		res += fourSumCount(A, B, C, D, index, order);
-        		index[order]--;
-        	}
+        else {
+            if (sum == 0)
+                res++;
+            while (order < 4) {
+                index[order]++;
+                res += fourSumCount(A, B, C, D, index, order);
+                index[order++]--;
+            }
         }
         return res;
     }
-    
-    //�ǵݹ��֦
+
+    //非递归剪枝
     public int fourSumCount2(int[] A, int[] B, int[] C, int[] D) {
-    	Arrays.sort(A);
+        Arrays.sort(A);
         Arrays.sort(B);
         Arrays.sort(C);
         Arrays.sort(D);
         int n = A.length;
         int res = 0;
-        for(int i = 0; i < n; i++)
-        	for(int j = 0; j < n; j++)
-        		for(int k = 0; k < n; k++)
-        			for(int l = 0; l < n; l++) {
-        				int sum = A[i] + B[j] + C[k] + D[l];
-        				if(sum == 0)
-        					res++;
-        				else if(sum > 0)
-        					break;
-        			}
-    	
-    	return res;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    for (int l = 0; l < n; l++) {
+                        int sum = A[i] + B[j] + C[k] + D[l];
+                        if (sum == 0) {
+                            res++;
+                        } else if (sum > 0) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return res;
     }
-    
+
+    //O(n^2)
     public int fourSumCount3(int[] A, int[] B, int[] C, int[] D) {
-    	Map<Integer, Integer> map = new HashMap<>();
+        //如果使用两个int[n*n]记录，再映射相加判断是否为0，为 O(n^4)
+        Map<Integer, Integer> map = new HashMap<>();
         int res = 0;
-        for(int i: A)
-            for(int j: B){
+        for (int i : A)
+            for (int j : B) {
                 map.put(-i - j, map.getOrDefault(-i - j, 0) + 1);
             }
 
-        for(int i: C)
-            for(int j: D){
+        for (int i : C)
+            for (int j : D) {
                 res += map.getOrDefault(i + j, 0);
             }
-        
+
         return res;
     }
-    
+
 }
